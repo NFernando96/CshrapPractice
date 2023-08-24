2ae476526da24ab45b348681067048dfb6319886
FERNANDO K.B.R.N <kbrnfernando96@outlook.com>
2023-08-25 00:09:16 +0530

    Delegates, Multithreading, Async

---
+++ CshrapTutorial/Part_30.cs
@@ -0,0 +1,38 @@
+﻿namespace CshrapTutorial
+{
+    internal class Part_30
+    {
+        public static void CheckGraterThan7(int num, Predicate<int> handler)
+        {
+            var result = handler(num);
+            Console.WriteLine(result);
+        }
+
+        public static void PrintSum(int a, int b, Func<int, int, int> delHandler)
+        {
+            Console.WriteLine($"Sum of {a} and {b} is {delHandler(a, b)}");
+        }
+
+        public static void Greet(string message, Action<string> greetHandler)
+        {
+            greetHandler(message);
+        }
+
+        static void Main()
+        {
+            Func<int, int, int> sumHandler = (a, b) => { return a + b; };
+            PrintSum(1, 2, sumHandler);
+
+            Action<string> greetHandler = (msg) => Console.WriteLine(msg);
+
+            Greet("Hello Naleen", greetHandler);
+
+            Predicate<int> handler = (num) =>
+            {
+                return num < 7;
+            };
+
+            CheckGraterThan7(10, handler);
+        }
+    }
+}
---
+++ CshrapTutorial/Part_31.cs
@@ -0,0 +1,36 @@
+﻿namespace CshrapTutorial
+{
+    internal class Part_31
+    {
+        static void Print1()
+        {
+            for (int i = 1; i < 5; i++)
+            {
+                Console.WriteLine("Method 1 - " + i);
+
+                if (i == 2)
+                {
+                    Thread.Sleep(5000);
+                }
+            }
+        }
+
+        static void Print2()
+        {
+            for (int i = 1; i < 5; i++)
+            {
+                Console.WriteLine("Method 2 - " + i);
+            }
+        }
+        static void Main()
+        {
+            //Print1();
+            //Print2();
+
+            Thread t1 = new Thread(new ThreadStart(Print1));
+            Thread t2 = new Thread(new ThreadStart(Print2));
+            t1.Start();
+            t2.Start();
+        }
+    }
+}
