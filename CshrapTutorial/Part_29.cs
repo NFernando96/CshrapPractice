9d7e0b35391fe13864aa9ab72c06ea4873c39512
FERNANDO K.B.R.N <kbrnfernando96@outlook.com>
2023-08-24 15:00:20 +0530

    Used lamda functions

--- CshrapTutorial/Part_29.cs
+++ CshrapTutorial/Part_29.cs
@@ -3,10 +3,10 @@
     delegate int SumDel(int n1, int n2);
     internal class Part_29
     {
-        //public static int Sum(int a, int b)
-        //{
-        //    return a + b;
-        //}
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
 
         public static void PrintSum(int a, int b, SumDel delhandler)
         {
@@ -17,11 +17,16 @@
         static void Main()
         {
             //SumDel sumHandler = Sum;
-            //SumDel sumHandler = delegate (int a, int b)
-            //{
-            //    return (a + b);
-            //};
+            //Using annonymous method
+            SumDel sumHandler = delegate (int a, int b)
+            {
+                return (a + b);
+            };
             PrintSum(10, 23, sumHandler);
+
+            //Using lamda functions
+            SumDel sumHandler1 = (a, b) => { return (a + b); };
+            PrintSum(20, 56, sumHandler1);
         }
     }
 }
